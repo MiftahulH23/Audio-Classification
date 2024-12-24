ccf6a55655d8fd41bafefb869cb5d15d75a0451c
MiftahulH23 <miftahul21si@mahasiswa.pcr.ac.id>
2024-12-24 17:16:28 +0700

    Update Header component; rename menu items and enhance navbar layout

--- web/app/_components/Header.jsx
+++ web/app/_components/Header.jsx
@@ -36,10 +36,13 @@ const Header = () => {
             className="menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-box bg-base-100 p-2 shadow"
           >
             <li>
-              <a>Homepage</a>
+              <a>Home</a>
             </li>
             <li>
-              <a>Portfolio</a>
+              <a>Label</a>
+            </li>
+            <li>
+              <a>Prediksi</a>
             </li>
             <li>
               <a>About</a>
@@ -49,42 +52,11 @@ const Header = () => {
       </div>
 
       {/* Navbar End for larger devices */}
-      <div className="navbar-end hidden sm:flex">
-        <button className="btn btn-circle btn-ghost">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
-          </svg>
-        </button>
-        <button className="btn btn-circle btn-ghost">
-          <div className="indicator">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-              />
-            </svg>
-            <span className="badge indicator-item badge-primary badge-xs"></span>
-          </div>
-        </button>
+      <div className="navbar-end hidden sm:flex font-semibold text-black flex gap-5">
+        <a href="#home">Home</a>
+        <a href="#label">Label</a>
+        <a href="#prediks">Prediksi</a>
+        <a href="#about">About</a>
       </div>
     </div>
   )
